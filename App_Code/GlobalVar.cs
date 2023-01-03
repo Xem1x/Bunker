4d6772dce9a5518be17f2960424e8f2a2c8fe0e4
Xem1x <tsyganok4@gmail.com>
2023-01-03 22:11:38 +0200

    List of players

--- App_Code/GlobalVar.cs
+++ App_Code/GlobalVar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BUNKER.GameData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,17 @@ namespace BUNKER
     public static class GlobalVar
     {
         static int i = 0;
+        static List<Player> players= new List<Player>();
+
+        public static void SetPlayers(Player inpt_player)
+        {
+            players.Add(inpt_player);
+        }
+        public static List<Player> GetPlayers()
+        {
+           return players;
+        }
+
         public static int I
         {
             get
