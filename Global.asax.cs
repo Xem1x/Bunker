1ccd53ddf491cdcec557c735f504d7c6a18b6437
Xem1x <tsyganok4@gmail.com>
2023-01-03 23:10:15 +0200

    count of users

--- Global.asax.cs
+++ Global.asax.cs
@@ -16,6 +16,29 @@ namespace BUNKER
             // Код, выполняемый при запуске приложения
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["TotalOnlineUsers"] = 0;
+        }
+        void Application_End(object sender, EventArgs e)
+        {
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["TotalOnlineUsers"] = (int)Application["TotalOnlineUsers"] + 1;
+            Application.UnLock();
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["TotalOnlineUsers"] = (int)Application["TotalOnlineUsers"] - 1;
+            Application.UnLock();
         }
     }
 }
